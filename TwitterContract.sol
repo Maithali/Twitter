b1e6e6af300b7f696090e2d21a3bec4ac8cb2565
Maithali <ghardem1@gmail.com>
2024-09-06 15:24:27 +0530

    adding more code

--- TwitterContract.sol
+++ TwitterContract.sol
@@ -34,6 +34,6 @@ contract TwitterContract {
     
     function _tweet(address _from, string memory _content) internal { // tweet  access check - owner, authority
      require(_from == msg.sender || operators[_from][msg.sender],"you dont have access");
-     tweets[nextId]=Tweet(nextId,
+     tweets[nextId]=Tweet(nextId,_from,
 
 }
